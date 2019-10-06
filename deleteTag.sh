fe5222ac46b9ca0c96f88a355462858886faca06
Aleksander Kovač <aleksander.kovac@gmail.com>
2019-10-06 02:25:52 +0200

    Testing scripts

--- deleteTag.sh
+++ deleteTag.sh
@@ -1,12 +1,13 @@
 echo "This script will delete a tag both locally and on github. The following tags are available on the current branch:"
 git tag -l
+branchName=$(git rev-parse --abbrev-ref HEAD)
 echo
 echo "Enter the name of the tag which will be deleted:"
 read tagName
 echo
-echo "Deleting $tagName ..."
+echo "Deleting $tagName on $branchName ..."
 set -x
-git push --delete origin $tagName
+git push -u origin --delete $tagName
 git tag --delete $tagName
 set +x
 echo
